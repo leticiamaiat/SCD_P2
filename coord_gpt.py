ba16acec706b4c24363d3332684a17c1052d9f49
RodrigoKasama <rodrigo.parracho@gmail.com>
2024-06-03 22:01:14 -0300

    Analise e Preparação do Ambiente

--- coord_gpt.py
+++ coord_gpt.py
@@ -1,59 +1,69 @@
-import socket
-import threading
-import queue
-import time
+from basecode import *
 
 # Fila de pedidos
 request_queue = queue.Queue()
 log = []
+num_clients = 5
 
-# Estrutura de dados para armazenar os sockets dos processos
-process_sockets = {}
+process_sockets = {}  # Estrutura de dados para armazenar os sockets dos processos
 
-# Função para tratar novos processos
+
+# TODO: Converter essas funções em métodos de uma classe
 def handle_new_connection(server_socket):
+    # Função para tratar novos processos
     while True:
         client_socket, addr = server_socket.accept()
-        process_id = addr[1]  # Usar a porta como identificador do processo (apenas exemplo)
+        # Usar a porta como identificador do processo (apenas exemplo)
+        process_id = addr[1]
         process_sockets[process_id] = client_socket
-        threading.Thread(target=handle_process, args=(client_socket, process_id)).start()
+        threading.Thread(target=handle_process, args=(
+            client_socket, process_id)).start()
 
-# Função para tratar mensagens de um processo
+
 def handle_process(client_socket, process_id):
+    # Função para tratar mensagens de um processo
     while True:
-        msg = client_socket.recv(10).decode()
+        msg = client_socket.recv(package_size).decode()
         if msg.startswith('1|'):  # REQUEST
             request_queue.put(process_id)
             log.append((time.time(), 'REQUEST', process_id))
+        elif msg.startswith('2|'):
+            # TODO: Lidar com o GRANT
+            print()
         elif msg.startswith('3|'):  # RELEASE
             log.append((time.time(), 'RELEASE', process_id))
 
-# Função para executar o algoritmo de exclusão mútua
+
 def exclusion_algorithm():
+    # Função para executar o algoritmo de exclusão mútua -- ?????
     while True:
         if not request_queue.empty():
             process_id = request_queue.get()
             client_socket = process_sockets[process_id]
-            client_socket.send('2|GRANT'.ljust(10).encode())
+            client_socket.send('2|GRANT'.ljust(package_size).encode())
             log.append((time.time(), 'GRANT', process_id))
 
-# Função para comandos do terminal
+
 def terminal_interface():
+    # Função para comandos do terminal
     while True:
-        cmd = input()
+        cmd = input("Aguardando entrada: ")
         if cmd == '1':
             print("Fila de pedidos:", list(request_queue.queue))
         elif cmd == '2':
-            count = {pid: log.count(('GRANT', pid)) for pid in process_sockets.keys()}
+            count = {pid: log.count(('GRANT', pid))
+                     for pid in process_sockets.keys()}
             print("Contagem de atendimentos:", count)
         elif cmd == '3':
             break
 
+
 # Início do servidor coordenador
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('localhost', 12345))
-server_socket.listen(5)
+server_socket.bind(host_addr)
+server_socket.listen(num_clients)
 
+# TODO: Não estamos armazenado nenhuma das threads !!
 # Threads do coordenador
 threading.Thread(target=handle_new_connection, args=(server_socket,)).start()
 threading.Thread(target=exclusion_algorithm).start()
--- process_gpt.py
+++ process_gpt.py
@@ -1,28 +1,40 @@
-import socket
-import time
+from basecode import *
+import random
 
-def access_critical_section(process_id, coordinator_ip, coordinator_port, r, k):
+# TODO: Rever esse papel todo - Muito confuso
+def access_critical_section(process_id, coordinator_ip, r, k):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((coordinator_ip, coordinator_port))
-    
+    client_socket.connect(coordinator_ip)
+
     for _ in range(r):
         # Envia REQUEST
-        client_socket.send('1|{}|000000'.format(process_id).ljust(10).encode())
-        grant_msg = client_socket.recv(10).decode()
+        client_socket.send('1|{}|000000'.format(
+            process_id).ljust(package_size).encode())
+
+        # Escuta a resposta
+        grant_msg = client_socket.recv(package_size).decode()
+
+        # Caso recebeu um GRANT
         if grant_msg.startswith('2|'):  # Recebe GRANT
+            # TODO: Isso não deve fazer parte do papel do processo, quem vai gerenciar isso é o coord
             with open('resultado.txt', 'a') as f:
-                f.write('{}|{}\n'.format(process_id, time.time()))
-            time.sleep(k)
-            # Envia RELEASE
-            client_socket.send('3|{}|000000'.format(process_id).ljust(10).encode())
-    
+                f.write('{}|{}\n'.format(process_id, int(time.time())))
+
+            # "Utilização" do serviço
+            time.sleep(k*random.randint(1, 3))
+
+            # Após, envia o RELEASE
+            client_socket.send('3|{}|000000'.format(
+                process_id).ljust(process_id).encode())
+
     client_socket.close()
 
+
 # Exemplo de execução do processo
-process_id = 1  # Identificador do processo
-coordinator_ip = 'localhost'
-coordinator_port = 12345
-r = 5  # Número de repetições
+process_id = 7  # Identificador do processo
+# coordinator_ip = 'localhost'
+# coordinator_port = 12345
+r = 25  # Número de repetições
 k = 2  # Tempo na região crítica
 
-access_critical_section(process_id, coordinator_ip, coordinator_port, r, k)
+access_critical_section(process_id, host_addr, r, k)
